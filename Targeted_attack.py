9195377b27d3402437fa3d8c83d395987d376486
Tejaswini Medi <meditejaswini2@gmail.com>
2023-09-23 19:05:33 +0200

    Update Targeted_attack.py

--- Targeted_attack.py
+++ Targeted_attack.py
@@ -3,80 +3,40 @@ import torch.nn.functional as F
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
+
 from utils import normalize_cifar
 
-class CrossEntropyLossManual:
-    """
-    y0 is the vector with shape (batch_size,C)
-    x shape is the same (batch_size), whose entries are integers from 0 to C-1
-    """
-    def __init__(self, ignore_index=-100) -> None:
-        self.ignore_index=ignore_index
-    
-    def __call__(self, y0, x):
-        loss = 0.
-        n_batch, n_class = y0.shape
-        for y1, x1 in zip(y0, x):
-            class_index = int(x1.item())
-            if class_index == self.ignore_index:
-                n_batch -= 1
-                continue
-            loss = loss + torch.log(torch.exp(y1[class_index])/(torch.exp(y1).sum()))
-        loss = - loss/n_batch
-        return loss
-
-
-def attack_pgd(
-        model,
-        x,
-        y,
-        y_t,
-        eps,
-        beta,
-        alpha,
-        n_iters,
-        dataset = 'cifar10',
-        norm = 'Linf',
-        **kwargs
-    ):
+def attack_pgd(model, x, y, eps, alpha, n_iters, dataset='cifar10' ,norm='Linf'):
     delta = torch.zeros_like(x).to(x.device)
     if norm == 'Linf':
         delta.uniform_(-eps, eps)
     else:
         raise ValueError
     delta = torch.clamp(delta, 0-x, 1-x)
+
     delta.requires_grad = True
     for _ in range(n_iters):
         if dataset == 'cifar10':
             output = model(normalize_cifar(x+delta))
         else:
             raise ValueError
-        loss = F.cross_entropy(output, y)  
+        loss = F.cross_entropy(output, y)
         loss.backward()
         grad = delta.grad.detach()
-        d = torch.clamp(delta + alpha * torch.sign(grad), min=-eps, max=eps)    #UT 
+        d = torch.clamp(delta + alpha * torch.sign(grad), min=-eps, max=eps)
         d = torch.clamp(d, 0 - x, 1 - x)
         delta.data = d
-        delta.grad.zero_()  
+        delta.grad.zero_()
+    
     return delta.detach()
 
-
-def cw_attack_pgd(
-        model,
-        x,
-        y,
-        y_t,
-        eps,
-        alpha,
-        n_iters,
-        norm = "Linf",
-        **kwargs
-    ):
-    delta = torch.zeros_like(x).to(x.device)   # size of the input
-    base = torch.ones_like(x).to(x.device)      # size of the input
+def cw_attack_pgd(model, x, y, eps, alpha, n_iters,norm='Linf'):
+    delta = torch.zeros_like(x).to(x.device)
+    base = torch.ones_like(x).to(x.device)
     for sample in range(len(x)):
         base[sample] *= eps[sample]
     eps = base.clone().to(x.device)
+
     if norm == 'Linf':
         delta = (torch.rand_like(delta) - 0.5) * 2
         delta.to(x.device)
@@ -85,75 +45,33 @@ def cw_attack_pgd(
         raise ValueError
 
     delta = torch.clamp(delta, 0-x, 1-x)
-    delta.requires_grad = True   ##### clamp and learnable parameter (noise)
+    delta.requires_grad = True
     for _ in range(n_iters):
         output = model(normalize_cifar(x+delta))
-        loss = F.cross_entropy(output, y_t)     ## Targeted loss
+        loss = F.cross_entropy(output, y)
         loss.backward()
-        grad = delta.grad.detach()    # calculate the noise gradient and detach
-        d = torch.clamp(delta - alpha * torch.sign(grad), min=-eps, max=eps)    #Targeted 
+        grad = delta.grad.detach()
+        d = torch.clamp(delta + alpha * torch.sign(grad), min=-eps, max=eps)
         d = torch.clamp(d, 0 - x, 1 - x)
         delta.data = d
-        delta.grad.zero_()    
-    return delta.detach()   # paramter learned for every pixel value
+        delta.grad.zero_()
+    
+    return delta.detach()
 
-def pgd_loss(
-        model,
-        x,
-        y,
-        y_t,
-        eps,
-        beta,
-        alpha,
-        n_iters = 10,
-        **kwargs
-    ):
-    delta = attack_pgd(
-        model,
-        x,
-        y,
-        y_t,
-        eps,
-        alpha,
-        n_iters
-    )
+def pgd_loss(model, x, y, eps, beta, alpha, n_iters=10):
+    delta = attack_pgd(model, x, y, eps, alpha, n_iters)
     robust_output = model(normalize_cifar(x + delta))
     criterion = nn.CrossEntropyLoss()
-    return (
-        criterion(robust_output, y),
-        robust_output.clone().detach()
-    )
+    return criterion(robust_output, y), robust_output.clone().detach()
 
-
-def cw_pgd_loss(
-        model,
-        x,
-        y,
-        y_t,
-        eps,
-        beta,
-        alpha,
-        n_iters = 10,
-        **kwargs
-    ):
-    batch_eps = eps[y] # change this
-    delta = cw_attack_pgd(
-        model,
-        x,
-        y,
-        y_t,
-        batch_eps,
-        alpha,
-        n_iters
-    )
+def cw_pgd_loss(model, x, y, cw_eps, beta, alpha, n_iters=10):
+    batch_eps = cw_eps[y]
+    delta = cw_attack_pgd(model, x, y, batch_eps, alpha, n_iters)
     robust_output = model(normalize_cifar(x + delta))
-    criterion = CrossEntropyLossManual()
-    return (
-        criterion(robust_output, y),
-        robust_output.clone().detach()
-    )
+    criterion = nn.CrossEntropyLoss()
+    return criterion(robust_output, y), robust_output.clone().detach()
 
-def trades_loss(model, x_natural, y,y_t, epsilon, cw_beta, step_size=0.003, perturb_steps=10):
+def trades_loss(model, x_natural, y, epsilon, cw_beta, step_size=0.003, perturb_steps=10):
     # define KL-loss
     criterion_kl = nn.KLDivLoss(size_average=False)
     model.eval()
@@ -184,11 +102,12 @@ def trades_loss(model, x_natural, y,y_t, epsilon, cw_beta, step_size=0.003, pert
     batch_beta = cw_beta[y]
     assert len(batch_beta) == len(loss_robust)                                                
     loss_robust = loss_robust.sum(1)
+    #print(batch_beta.shape, loss_natural.shape, loss_robust.shape)
     loss = ((1-batch_beta) * loss_natural + batch_beta * loss_robust).sum()
 
     return loss, robust_out.clone().detach()
 
-def cw_trades_loss(model, x_natural, y,y_t, cw_eps, cw_beta, step_size, perturb_steps):
+def cw_trades_loss(model, x_natural, y, cw_eps, cw_beta, step_size, perturb_steps):
     batch_beta = cw_beta[y]
     batch_eps = cw_eps[y]
     base = torch.ones_like(x_natural).to(x_natural.device)
@@ -225,11 +144,12 @@ def cw_trades_loss(model, x_natural, y,y_t, cw_eps, cw_beta, step_size, perturb_
 
     assert len(batch_beta) == len(loss_robust)                                                
     loss_robust = loss_robust.sum(1)
+    #print(batch_beta.shape, loss_natural.shape, loss_robust.shape)
     loss = ((1-batch_beta) * loss_natural + batch_beta * loss_robust).sum()
 
     return loss, robust_out.clone().detach()
 
-def fat_loss(model, x, y,y_t, eps, tau, alpha, n_iters=10):
+def fat_loss(model, x, y, eps, tau, alpha, n_iters=10):
     K = n_iters
     control = (torch.ones(len(x)) * tau).to(x.device)
     delta = torch.zeros_like(x).to(x.device)
@@ -274,7 +194,7 @@ def fat_loss(model, x, y,y_t, eps, tau, alpha, n_iters=10):
     criterion = nn.CrossEntropyLoss()
     return criterion(robust_output, y), robust_output.clone().detach()
 
-def cw_fat_loss(model, x, y,y_t, cw_eps, tau, alpha, n_iters=10):
+def cw_fat_loss(model, x, y, cw_eps, tau, alpha, n_iters=10):
     batch_eps = cw_eps[y]
     delta = torch.zeros_like(x).to(x.device)
     base = torch.ones_like(x).to(x.device)
