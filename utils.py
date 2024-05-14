3732486298df10116c509426b040bb30b9bf1749
Tejaswini Medi <meditejaswini2@gmail.com>
2024-05-14 15:39:46 +0200

    Updated utils.py for CIFAR100C

--- utils.py
+++ utils.py
@@ -258,3 +258,31 @@ class CIFAR10C(datasets.VisionDataset):
     def __len__(self):
         return len(self.data)
 
+class CIFAR100C(datasets.VisionDataset):
+    def __init__(self, root :str, name :str,corruptions:str,
+                 transform=None, target_transform=None):
+        assert name in corruptions
+        super(CIFAR100C, self).__init__(
+            root, transform=transform,
+            target_transform=target_transform
+        )
+        data_path = os.path.join(root, name + '.npy')
+        target_path = os.path.join(root, 'labels.npy')
+        
+        self.data = np.load(data_path)
+        self.targets = np.load(target_path)
+        
+    def __getitem__(self, index):
+        img, targets = self.data[index], self.targets[index]
+        img = Image.fromarray(img)
+        
+        if self.transform is not None:
+            img = self.transform(img)
+        if self.target_transform is not None:
+            targets = self.target_transform(targets)
+            
+        return img, targets
+    
+    def __len__(self):
+        return len(self.data)
+
